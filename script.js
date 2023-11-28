e9316d1e8c5e3fb8c7e6615c895ceb3d3e095cf6
Khan Faiz <khan.faiz.ayub@gmail.com>
2023-11-28 15:45:20 +0530

    if else

---
+++ script.js
@@ -0,0 +1,20 @@
+// /*
+let age=prompt('Enter your age')
+age=Number.parseInt(age)
+if(age>10 && age<20){
+    console.log('your age lies betwwen 10 and 20')
+}else{
+    console.log('your age dosnt lies betwwen 10 and 20')
+}
+// */
+
+
+
+
+
+
+
+
+
+
+
