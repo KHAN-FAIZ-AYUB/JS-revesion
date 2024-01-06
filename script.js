c82b71036246a13b41ed71d3ef50a0dfb63114bd
Khan Faiz <khan.faiz.ayub@gmail.com>
2024-01-06 20:23:24 +0530

    replaceWith

--- script.js
+++ script.js
@@ -494,11 +494,12 @@ console.log(first.dataset.game);
 // ---------------------VIDEO 43--------------------------
 
 let a = document.getElementsByTagName("div")[0];
-a.innerHTML = a.innerHTML + "<h1>Hello World</h1>";
+// a.innerHTML = a.innerHTML + "<h1>Hello World</h1>";
 
 let div = document.createElement("div");
 div.innerHTML = "<h2>HEY</h2>";
 // a.appendChild(div)
 // a.prepend(div);
 // a.before(div);
-a.after(div);
+// a.after(div);
+a.replaceWith(div);
