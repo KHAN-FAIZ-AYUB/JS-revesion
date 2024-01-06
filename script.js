f7c9f33fed52f3b10650ef2693d4c937f1bca49a
Khan Faiz <khan.faiz.ayub@gmail.com>
2024-01-06 20:17:43 +0530

    prepend

--- script.js
+++ script.js
@@ -497,5 +497,6 @@ let a = document.getElementsByTagName("div")[0];
 a.innerHTML = a.innerHTML + "<h1>Hello World</h1>";
 
 let div = document.createElement("div");
-div.innerHTML = '<h2>HEY</h2>';
-a.appendChild(div) 
+div.innerHTML = "<h2>HEY</h2>";
+// a.appendChild(div)
+a.prepend(div);
