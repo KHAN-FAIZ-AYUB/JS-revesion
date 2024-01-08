1e3e2f50b970acb492201a3139a49b369705e9e5
Khan Faiz <khan.faiz.ayub@gmail.com>
2024-01-09 01:12:27 +0530

    setinterval-toggle

--- script.js
+++ script.js
@@ -629,3 +629,40 @@ btn.addEventListener("click", a);
 */
 
 // ---------------------VIDEO 49--------------------------
+/*
+let google = document
+  .getElementById("google")
+  .addEventListener("click", function () {
+    window.location = "https://google.com";
+    window.focus();
+  });
+
+let github = document
+  .getElementById("github")
+  .addEventListener("click", function () {
+    window.location = "https://github.com";
+    window.focus();
+  });
+
+let youtube = document
+  .getElementById("youtube")
+  .addEventListener("click", function () {
+    window.location = "https://youtube.com";
+    window.focus();
+  });
+
+const fetchContent = async (url) => {
+  con = await fetch(url);
+  let a = await con.json();
+  return a;
+};
+
+setInterval(async function () {
+  let url = "https://jsonplaceholder.typicode.com/todos/1";
+  // console.log(await fetchContent(url));
+}, 3000);
+*/
+
+setInterval(async function () {
+  document.querySelector("#bulb").classList.toggle("bulb");
+}, 100);
