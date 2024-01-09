b50d71383a56041765bf8b687c99108571db6842
Khan Faiz <khan.faiz.ayub@gmail.com>
2024-01-09 15:13:40 +0530

    Syncronous

--- script.js
+++ script.js
@@ -671,3 +671,14 @@ setInterval(async function () {
 */
 
 // ---------------------VIDEO 50--------------------------
+
+// ---------------------VIDEO 51--------------------------
+// --------------------Syncronous-------------------------
+
+let name = prompt("What is your name");
+let age = prompt("What is your age");
+let bike = prompt("Which is your favourate bike");
+let car = prompt("Which is your favourate car");
+console.log(
+  `${name}  is ${age}  years old and his fav bike is ${bike} aNd his fav car is ${car}`
+);
