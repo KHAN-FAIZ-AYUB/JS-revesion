241070b82fb1901c3d0d079c3d3fd7773b0069bf
Khan Faiz <khan.faiz.ayub@gmail.com>
2024-01-12 04:01:05 +0530

    Promise.race

--- script.js
+++ script.js
@@ -889,13 +889,13 @@ p1.then(() => {
 let p1 = new Promise((resolve, reject) => {
   setTimeout(() => {
     resolve("value 1");
-  }, 1000);
+  }, 5000);
 });
 
 let p2 = new Promise((resolve, reject) => {
   setTimeout(() => {
-    // resolve("value 2");
-    reject(new Error("ERROR 404"));
+    resolve("value 2");
+    // reject(new Error("ERROR 404"));
   }, 2000);
 });
 
@@ -918,11 +918,11 @@ let p3 = new Promise((resolve, reject) => {
 // });
 
 // let promise_all = Promise.all([p1, p2, p2]);
-let promise_all = Promise.allSettled([p1, p2, p3]);
-promise_all
-  .then((value) => {
-    console.log(value);
-  })
-  // .catch((error) => {
-  //   console.log(error);
-  // });
+// let promise_all = Promise.allSettled([p1, p2, p3]);
+let promise_all = Promise.race([p1, p2, p3]);
+promise_all.then((value) => {
+  console.log(value);
+});
+// .catch((error) => {
+//   console.log(error);
+// });
