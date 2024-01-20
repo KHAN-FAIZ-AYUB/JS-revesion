d149c853f71a38c07d7611da9098e63c77097603
Khan Faiz <khan.faiz.ayub@gmail.com>
2024-01-20 17:54:20 +0530

    localStorage.clear

--- script.js
+++ script.js
@@ -1224,3 +1224,6 @@ console.log(`The value at ${key} is ${localStorage.getItem(key)}`);
 if (key == "red" || key == "blue") {
   localStorage.removeItem(key);
 }
+if (key == 0) {
+  localStorage.clear();
+}
